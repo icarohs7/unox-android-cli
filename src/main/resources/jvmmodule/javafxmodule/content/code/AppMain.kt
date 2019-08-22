527ba34630f162861922f1c4b38ee3151fee809a
Icaro Temponi <icarohs7@gmail.com>
2019-08-22 09:46:34 -0300

    Add foregroundDispatcher configuration on AppMain of JavaFx app template
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- src/main/resources/jvmmodule/javafxmodule/content/code/AppMain.kt
+++ src/main/resources/jvmmodule/javafxmodule/content/code/AppMain.kt
@@ -1,12 +1,15 @@
 package {{module.completeName}}
 
 import {{module.completeName}}.presentation.views.main.MainView
+import com.github.icarohs7.unoxcore.UnoxCore
+import kotlinx.coroutines.Dispatchers
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 import tornadofx.*
 
 class AppMain : App(MainView::class) {
     init {
+        UnoxCore.foregroundDispatcher = Dispatchers.Main
         startKoin {
             modules(module {
             })
