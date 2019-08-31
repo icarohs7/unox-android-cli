93df4805e6bd9dc6ce04f0fb4b17055c5a964340
Icaro Temponi <icarohs7@gmail.com>
2019-08-31 20:19:41 -0300

    Change python scripts bundled with rootmodule template
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

---
+++ src/main/resources/rootmodule/pull_buildsrc.py
@@ -0,0 +1,5 @@
+from os import system
+
+system("git clone https://github.com/icarohs7/unox-buildsrc buildSrc")
+print("done")
+system("pause")
---
+++ src/main/resources/rootmodule/pull_resmodules.py
@@ -0,0 +1,5 @@
+from os import system
+
+system("git clone https://github.com/icarohs7/unox-android-resmodules resmodules")
+print("done")
+system("pause")
